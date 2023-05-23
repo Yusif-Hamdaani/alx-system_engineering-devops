037645ba16f0772c2d77a0790853732606315140
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2023-05-23 04:48:50 -0700

    All task

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,3 @@
 #!/usr/bin/env ruby
-
-# Use a more efficient regular expression with the case-insensitive flag
-regex = /school/i
-
-# Extract the first command line argument directly
-input_string = ARGV[0]
-
-# Use the String#scan method with the regular expression
-matches = input_string.scan(regex)
-
-# Join the matches using a space as a separator
-result = matches.join(' ')
-
-puts result
+# A regular expression that is simply matching School
+puts ARGV[0].scan(/School/).join
